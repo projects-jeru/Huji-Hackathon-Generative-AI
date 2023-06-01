574f14ded3ec678926582e5397f4cc354a22ceb9
avrymi-asraf <avreymi.asraf@gmail.com>
2023-06-01 15:32:21 +0300

    Co-authored-by: matankaminski <matankaminski@users.noreply.github.com> Co-authored-by: Dan Korenfeld <DanKorenfeld1@users.noreply.github.com>

--- pipline.py
+++ pipline.py
@@ -3,33 +3,74 @@ from langchain import PromptTemplate, OpenAI, LLMChain
 from langchain.prompts import load_prompt
 import wikipedia
 import os
+import streamlit as st
 
-llm = OpenAI(openai_api_key='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')
-# save templates to a file
-try_imply_template = """Question:
- The user wrote me the following text, what is he trying to imply to me?
- {user_input}
+import openai
 
-Answer: Let's think step by step."""
-# An example prompt with multiple input variables
-TI_prompt = PromptTemplate(
-    input_variables=["user_input"],
-    template=try_imply_template,
-)
+openai.api_key = "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"
+
+prompt = "i am learning python give me exercise to practice if-else"
+def llm(context,system="You are a helpful assistant."):
+    response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo-0301",
+    messages=[
+            {"role": "system", "content": system},
+            {"role": "user", "content": context},
+        ]
+    )
+    return response['choices'][0]['message']['content']
+
+# # save 
+generate_question_template =lambda topic: f"""Write a simple question in Python:
+The question should be in this topic:{topic}
+The question should be clear and simple, without any difficulties
+Let's think step by step, and write the code
+code: 
+```python
+
+"""
 
 
-connection_between_terms_template = PromptTemplate(
-    template="""Question:
- What is the connection between {term1} and {term2}?
- Answer: Let's think step by step.""",
-    input_variables=["term1", "term2"],
-)
+subtopic_template = lambda questoin, answer:f"""Kim answered the question {questoin},
+her answer was {answer}.
+The list of topics that Kim does not seem to know:
+1."""
 
 
-chain_TI = LLMChain(prompt=TI_prompt, llm=llm)
-chain_CC = LLMChain(prompt=connection_between_terms_template, llm=llm)
+next_question_template = lambda topic, subtopics:f"""Write a short code question that deals with this topic:{topic}
+and refers to these sub-topics:{subtopics}
+The question should be clear and simple, without any difficulties
+Let's think step by step, and write the code
+code: 
+```python"""
+
+def generate_question_chain(topic):
+    return llm(generate_question_template(topic))
+def subtopic_chain(questoin,answer):
+    return llm(subtopic_template(questoin,answer))
+def next_question_chain(topic,subtopic):
+    return llm(next_question_template(topic,subtopic))
+
+
+def generate_question(user_data:dict)->str:
+    return generate_question_chain(topic=user_data["subject"])
+    
+
+
+def next_question(questoin:str,answer:str,user_dict:dict)->str:
+    subtopic = subtopic_chain(questoin=questoin, answer=answer)
+    return next_question_chain(topic = user_dict["subject"],subtopic=subtopic)
+
+
+
+
+
 
 
 if __name__ == "__main__":
-    print(chain_TI.run("I am happy"))
-    print(chain_CC.run(["I am sad", "I am happy"]))
+    user_profile = {"language": "python", "level" : None, "user_name" : None, "subject": "dictionary", "weaknesses": None}
+    q=generate_question(user_profile)
+    print("Q: "+q+"\n")
+    print(next_question(q,input("enter answer"),user_profile))
+
+
